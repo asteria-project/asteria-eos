c41b8e259e45136812d8d08c3c898fa1a9c61cd6
Pascal ECHEMANN <pechemann@gmail.com>
2019-07-14 10:34:00 +0200

    API update
    Add new Helios HTTP error codes.

--- dist/com/asteria/eos/util/error/HeliosHttpErrorCode.js
+++ dist/com/asteria/eos/util/error/HeliosHttpErrorCode.js
@@ -2,4 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var HeliosHttpErrorCode;
 (function (HeliosHttpErrorCode) {
+    HeliosHttpErrorCode["ERR_INTERNAL_PROCESS_FAILURE"] = "INTERNAL_PROCESS_FAILURE";
+    HeliosHttpErrorCode["ERR_DIRECTORY_LISTING_FAILED"] = "DIRECTORY_LISTING_FAILED";
+    HeliosHttpErrorCode["ERR_RESOURCE_NOT_FOUND"] = "RESOURCE_NOT_FOUND";
+    HeliosHttpErrorCode["ERR_TARGET_IS_A_DIRECTORY"] = "TARGET_IS_A_DIRECTORY";
+    HeliosHttpErrorCode["ERR_MISSING_CONTENT_TYPE"] = "MISSING_CONTENT_TYPE";
+    HeliosHttpErrorCode["ERR_UNSUPPORTED_CONTENT_TYPE"] = "UNSUPPORTED_CONTENT_TYPE";
+    HeliosHttpErrorCode["ERR_MULTIPART_BOUNDARY_NOT_FOUND"] = "MULTIPART_BOUNDARY_NOT_FOUND";
 })(HeliosHttpErrorCode = exports.HeliosHttpErrorCode || (exports.HeliosHttpErrorCode = {}));
--- src/com/asteria/eos/util/error/HeliosHttpErrorCode.ts
+++ src/com/asteria/eos/util/error/HeliosHttpErrorCode.ts
@@ -4,4 +4,38 @@
  */
 export enum HeliosHttpErrorCode {
 
+    /**
+     * Means that the server failed to execute the current process and no error code has been identified yet.
+     */
+    ERR_INTERNAL_PROCESS_FAILURE = 'INTERNAL_PROCESS_FAILURE',
+
+    /**
+     * Means that the server failed to retrieve the listing of a directory.
+     */
+    ERR_DIRECTORY_LISTING_FAILED = 'DIRECTORY_LISTING_FAILED',
+
+    /**
+     * Means that the server failed to retrieve the specified resource.
+     */
+    ERR_RESOURCE_NOT_FOUND = 'RESOURCE_NOT_FOUND',
+    
+    /**
+     * Means that the current target is a directory while a file is expected.
+     */
+    ERR_TARGET_IS_A_DIRECTORY = 'TARGET_IS_A_DIRECTORY',
+
+    /**
+     * Means that the content type of an HTTP request is missing.
+     */
+    ERR_MISSING_CONTENT_TYPE = 'MISSING_CONTENT_TYPE',
+    
+    /**
+     * Means that the content type of an HTTP request is not supported.
+     */
+    ERR_UNSUPPORTED_CONTENT_TYPE = 'UNSUPPORTED_CONTENT_TYPE',
+    
+    /**
+     * Means that the multipart definition of an HTTP request is missing.
+     */
+    ERR_MULTIPART_BOUNDARY_NOT_FOUND = 'MULTIPART_BOUNDARY_NOT_FOUND'
 }
